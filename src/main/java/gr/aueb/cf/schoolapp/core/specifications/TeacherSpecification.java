d6a4b8f256424e49c6dc126b6adb72da2748b9e9
Markos Stinis <mark93@hotmail.gr>
2024-12-11 15:31:05 +0200

    Initial commit

---
+++ src/main/java/gr/aueb/cf/schoolapp/core/specifications/TeacherSpecification.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.schoolapp.core.specifications;
+
+import gr.aueb.cf.schoolapp.model.Teacher;
+import org.springframework.data.jpa.domain.Specification;
+
+public class TeacherSpecification {
+
+    private TeacherSpecification() {
+
+    }
+
+    public static Specification<Teacher>
+}
